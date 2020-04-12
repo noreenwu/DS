642dc4d5885fa24e27b6ded375ec0588c6e70d36
Noreen Wu <noreenwu@gmail.com>
2020-04-12 14:53:16 -0400

    feat: added intersection and more tests

--- sets/union_intersection.py
+++ sets/union_intersection.py
@@ -17,6 +17,8 @@ class LinkedList:
         while cur_head:
             out_string += str(cur_head.value) + " -> "
             cur_head = cur_head.next
+        if out_string == "":
+            return "no elements in common"
         return out_string
 
 
@@ -44,14 +46,13 @@ class LinkedList:
 
 
 def union(llist_1, llist_2):
-    # Your Solution Here
     # loop thru each list and check a hash to see if item was seen before.
-    # only add it to final list if not seen before. add each item to hash.
+    # only add it to final list if not seen before. add each unseen item to hash.
 
     new_llist = LinkedList()
-    curr = llist_1.head
     dict = {}
 
+    curr = llist_1.head
     while curr:
         if dict.get(curr.value) is None:
             # add to new linked list
@@ -73,8 +74,26 @@ def union(llist_1, llist_2):
 
 def intersection(llist_1, llist_2):
     # Your Solution Here
-    pass
 
+    new_llist = LinkedList()
+    dict = {}
+
+    curr = llist_1.head
+    while curr:
+        if dict.get(curr.value) is None:
+            dict[curr.value] = 1
+
+        curr = curr.next
+
+    curr = llist_2.head
+    while curr:
+        if dict.get(curr.value) != None:
+            # add to new linked list
+            new_llist.append(curr.value)
+
+        curr = curr.next
+
+    return (new_llist)
 
 # Test case 1
 
@@ -92,23 +111,25 @@ for i in element_2:
 
 print(linked_list_1)
 print(linked_list_2)
-# union(linked_list_1, linked_list_2)
 print (union(linked_list_1,linked_list_2))
-# print (intersection(linked_list_1,linked_list_2))
+print (intersection(linked_list_1,linked_list_2))
 
 # # Test case 2
 
-# linked_list_3 = LinkedList()
-# linked_list_4 = LinkedList()
+linked_list_3 = LinkedList()
+linked_list_4 = LinkedList()
 
-# element_1 = [3,2,4,35,6,65,6,4,3,23]
-# element_2 = [1,7,8,9,11,21,1]
+element_1 = [3,2,4,35,6,65,6,4,3,23]
+element_2 = [1,7,8,9,11,21,1]
 
-# for i in element_1:
-#     linked_list_3.append(i)
+for i in element_1:
+    linked_list_3.append(i)
 
-# for i in element_2:
-#     linked_list_4.append(i)
+for i in element_2:
+    linked_list_4.append(i)
 
-# print (union(linked_list_3,linked_list_4))
-# print (intersection(linked_list_3,linked_list_4))
+print("lists 3 and 4")
+print(linked_list_3)
+print(linked_list_4)
+print (union(linked_list_3,linked_list_4))
+print (intersection(linked_list_3,linked_list_4))
