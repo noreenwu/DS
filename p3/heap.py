7f83d3d674c49e79a39803873b6e7329a05a6a61
Noreen Wu <noreenwu@gmail.com>
2020-04-15 20:49:16 -0400

    feat: build a tree based on the priority queue

--- p3/heap.py
+++ p3/heap.py
@@ -7,32 +7,62 @@ class Node(object):
         self.left_child = None
         self.right_child = None
 
-    # def __cmp__(self, other):
-    #     if self.value < other.value:
-    #         return -1
-    #     elif self.value > other.value:
-    #         return 1
-    #     else:
-    #         return 0
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def set_left_child(self, left):
+        self.left_child = left
+
+    def set_right_child(self, right):
+        self.right_child = right
+
+    def get_left_child(self):
+        return self.left_child
+
+    def get_right_child(self):
+        return self.right_child
+
+    def has_left_child(self):
+        return self.left != None
+
+    def has_right_child(self):
+        return self.right != None
+
+    def __repr__(self):
+        return f"Node({self.get_value()})"
 
     def __lt__(self, other):
         return (self.value < other.value)
 
-n1 = Node(1)
 
-n3 = Node(5)
+class Tree():
+    def __init__(self, value=None):
+        self.root = Node(value)
+
+    def get_root(self):
+        return self.root
+
+
+def traverse_tree(node):
+    
+    pass
+
+
+n1 = Node(1)
+print("node1 {}".format(n1))
 
 n2 = Node(2)
 
-n4 = Node(11)
+n3 = Node(3)
 
-n5 = Node(0)
+n4 = Node(4)
+
+n5 = Node(5)
 
 
-if n1 > n3:
-    print("n1 bigger")
-else:
-    print("n5 bigger")
 h = []
 
 heappush(h, n1)
@@ -41,13 +71,69 @@ heappush(h, n2)
 
 heappush(h, n3)
 
+heappush(h, n5)
+
 heappush(h, n4)
 
-heappush(h, n5)
+
+
+
+print("length of h is {}".format(len(h)))
+# print len(h)
+
+ordered = []
+while h:
+    popped = heappop(h)
+    print("popped {}".format(popped.value))
+    ordered.append(popped)
+
+print("after popping, length of h is {}".format(len(h)))
+
+node16 = Node(16)
+heappush(h, node16)
+for i in range(len(ordered)):
+    item = ordered.pop()
+    print("put back {}".format(item.value))
+
+    heappush(h, item)
+
+## take 2 nodes off heap and make a tree
+
+print("initial heap size is {}".format(len(h)))
+
+while len(h) > 1:
+    tree = Node(0)    
+    pop1 = heappop(h)
+    pop2 = heappop(h)
+    print("popping {} and {}".format(pop1, pop2))
+    print("just popped 2 items and heap len is {}".format(len(h)))
+    tree.left_child = pop1
+    tree.right_child = pop2
+    tree.set_value(pop1.get_value() + pop2.get_value())
+    print("new node is {}".format(tree))
+
+    heappush(h, tree)
+    print("just pushed 1 tree item and heap len is {}".format(len(h)))
+
+
+print("size of h is now: {}".format(len(h)))
+
+top = heappop(h)    
+print ("top node is {}".format(top.get_value()))    
+# if pop1 is not None and pop2 is not None:
+#     tree.left_child = pop1
+#     tree.right_child = pop2
+#     tree.value = pop1.value + pop2.value
+#     print("new value is {}".format(pop1.value + pop2.value))
+
+# heappush(h, tree)
+
+
 
 ordered = []
 while h:
     ordered.append(heappop(h))
+
 
 for o in ordered:
     print(o.value)
