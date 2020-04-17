fee3ff94e80ce5c0067ecae72b51a09d0c42a860
Noreen Wu <noreenwu@gmail.com>
2020-04-17 16:32:04 -0400

    clean/test: cleaned up debug code and test cases

--- p4/problem_4.py
+++ p4/problem_4.py
@@ -25,8 +25,14 @@ class Group(object):
 
         # check subgroups
         for g in self.groups:
-            print("checking subgroup {}".format(g))
-            return g.user_in_group(user)
+            print("   checking subgroup {}:\n".format(g.name))
+
+            # return g.user_in_group(user)
+            if g.user_in_group(user):
+                # print(" {} is returning True".format(g.name))
+                return True
+            # else:
+            #     print(" {} is  False, so keep going!".format(g.name))
 
         return False
 
@@ -46,7 +52,7 @@ class Group(object):
             str += "groups: {}\n".format(groups)
 
         if users != "":
-            str += "users: {}".format(users)
+            str += "users: {}\n\n".format(users)
 
         return (str)
 
@@ -59,24 +65,45 @@ def is_user_in_group(user, group):
       user(str): user name/id
       group(class:Group): group to check user membership against
     """
-    print("look for {} in {}\n".format(user, group))
+    print("Look for {} in {}\n".format(user, group.name))
 
-    return group.user_in_group(user)
+    result = group.user_in_group(user)
+    print("Is the user {} in group {}?  {} ".format(user, group.name, result))
+    return result
 
 
-parent_group = Group("parentgroup")
-child_group = Group("childgroup")
-parent_group.add_group(child_group)
+# test case 1
+parent_group = Group("Parent Group")
 
-child_user = "childuser"
-child_group.add_user(child_user)
+child_group_a = Group("Child Group A")
+parent_group.add_group(child_group_a)
 
-print(is_user_in_group("childuser", parent_group))
+child_group_b = Group("Child Group B")
+parent_group.add_group(child_group_b)
 
-sub_child_group = Group("subchildgroup")
-child_group.add_group(sub_child_group)
+child_group_aa = Group("Child Group AA")
+child_group_ab = Group("Child Group AB")
 
-sub_child_user = "subchilduser"
-sub_child_group.add_user(sub_child_user)
+child_group_a.add_group(child_group_aa)
+child_group_a.add_group(child_group_ab)
 
-print(is_user_in_group("subchilduser", parent_group))
+child_group_ba = Group("Child Group BA")
+child_group_bb = Group("Child Group BB")
+
+child_group_b.add_group(child_group_ba)
+child_group_b.add_group(child_group_bb)
+
+child_user_b = "childuserb"
+child_group_b.add_user(child_user_b)
+
+is_user_in_group(child_user_b, parent_group)  # user is found in Child Group B under Parent Group
+
+
+child_user_ba = "childuserba"
+child_group_ba.add_user(child_user_ba)
+
+is_user_in_group(child_user_ba, parent_group)  # user is found in Child Group BA under Child Group B under Parent Group
+
+is_user_in_group(child_user_ba, child_group_a) # user is NOT found in Child Group A (the user is in B)
+
+
