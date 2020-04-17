7adc17484627285c7887f9c45ddc743fb62e071b
Noreen Wu <noreenwu@gmail.com>
2020-04-16 20:57:22 -0400

    refactor: moved logic into functions

--- p3/huffman.py
+++ p3/huffman.py
@@ -1,6 +1,5 @@
 from heapq import heappush, heappop
 
-heap = []
 
 
 class Node(object):
@@ -51,19 +50,10 @@ class Node(object):
         return (self.value < other.value)
 
 
-def traverse(node, charstr):
-    if node:
-        if node.get_letter() != '':
-            visit_order.append(node.get_value())
-            node.set_code(charstr)
-            print("letter {} assigned {}".format(node.get_letter(), node.get_code()))
-            encode_table[node.get_letter()] = node.get_code()
 
-        traverse(node.get_left_child(), charstr+'0')
 
-        traverse(node.get_right_child(), charstr+'1')
 
-    return visit_order
+
 
 def decode(str, node):
     if node is None:
@@ -72,15 +62,12 @@ def decode(str, node):
     print ("decode {}".format(node.get_value()))
     res = ''    
     for s in str:
-        # print(s)
         if s == '0':
             if (curr.has_left_child()):
                 curr = curr.get_left_child()
-                # res += '0'
         elif s == '1':
             if (curr.has_right_child()):
                 curr = curr.get_right_child()
-                # res += '1'
 
         if curr.get_letter() != '':
             res += curr.get_letter()
@@ -90,63 +77,128 @@ def decode(str, node):
     return res
 
 
-
-
-dict = {}
-def count_letters(str):
-    for a in str:
+def huffman_build_tree(data):
+    # count frequency of letters
+    dict = {}    
+    for a in data:
         dict[a] = dict.get(a, 0) + 1
 
+    # create priority queue of nodes, using frequency data collected
+    heap = []
     for k, v in dict.items():
         node = Node(v, k)
         print ("new node {}".format(node))
         heappush(heap, node)
 
 
+    # create mini-trees with two lowest frequency values until all nodes are under one parent
+    while len(heap) > 1:
+        tree = Node(0)    
+        pop1 = heappop(heap)
+        pop2 = heappop(heap)
+        print("popping {} and {}".format(pop1, pop2))
+        print("just popped 2 items and heap len is {}".format(len(heap)))
+        tree.left_child = pop1
+        tree.right_child = pop2
+        tree.set_value(pop1.get_value() + pop2.get_value())
+        print("new node is {}".format(tree))
+
+        heappush(heap, tree)
+        print("just pushed 1 tree item and heap len is {}".format(len(heap)))
+
+
+    print("size of h is now: {}".format(len(heap)))
+
+    top = heappop(heap)    
+    print ("top node is {}".format(top.get_value()))        
+
+    return top  # encode/decode tree
+
+encode_table = {}  
+
+def encode(node, charstr):
+    visit_order = []    
+  
+
+    if node:
+        if node.get_letter() != '':
+            visit_order.append(node.get_value())
+            node.set_code(charstr)
+            print("letter {} assigned {}".format(node.get_letter(), node.get_code()))
+            encode_table[node.get_letter()] = node.get_code()
+
+        encode(node.get_left_child(), charstr+'0')
+
+        encode(node.get_right_child(), charstr+'1')
+
+    return encode_table
+
+
+def huffman_encode(data, tree):
+    ## traverse tree and create codes
+    print("huffman_encode")
+    encode_table = encode(top, "")
+
+    print("Encoding table: ")
+    for k, v in encode_table.items():
+        print("k {}: {}".format(k, v))
+
+
+    encoded = ""
+    for s in data:
+        print(s)
+        encoded += encode_table[s]
+
+    print(encoded)
+    return encoded
+
+def huffman_decode(data, tree):
+    pass
+
+
+
+# dict = {}
+# def count_letters(str):
+#     for a in str:
+#         dict[a] = dict.get(a, 0) + 1
+
+#     for k, v in dict.items():
+#         node = Node(v, k)
+#         print ("new node {}".format(node))
+#         heappush(heap, node)
+
+
 str_to_encode = "helllo"
-count_letters(str_to_encode)
+top = huffman_build_tree(str_to_encode)
 
-print("initial heap size is {}".format(len(heap)))
+encoded = huffman_encode(str_to_encode, top)
 
-while len(heap) > 1:
-    tree = Node(0)    
-    pop1 = heappop(heap)
-    pop2 = heappop(heap)
-    print("popping {} and {}".format(pop1, pop2))
-    print("just popped 2 items and heap len is {}".format(len(heap)))
-    tree.left_child = pop1
-    tree.right_child = pop2
-    tree.set_value(pop1.get_value() + pop2.get_value())
-    print("new node is {}".format(tree))
+# count_letters(str_to_encode)
 
-    heappush(heap, tree)
-    print("just pushed 1 tree item and heap len is {}".format(len(heap)))
+# print("initial heap size is {}".format(len(heap)))
 
+# while len(heap) > 1:
+#     tree = Node(0)    
+#     pop1 = heappop(heap)
+#     pop2 = heappop(heap)
+#     print("popping {} and {}".format(pop1, pop2))
+#     print("just popped 2 items and heap len is {}".format(len(heap)))
+#     tree.left_child = pop1
+#     tree.right_child = pop2
+#     tree.set_value(pop1.get_value() + pop2.get_value())
+#     print("new node is {}".format(tree))
 
-print("size of h is now: {}".format(len(heap)))
-
-top = heappop(heap)    
-print ("top node is {}".format(top.get_value()))    
+#     heappush(heap, tree)
+#     print("just pushed 1 tree item and heap len is {}".format(len(heap)))
 
 
-## traverse tree and create codes
-visit_order = []
-encode_table = {}
+# print("size of h is now: {}".format(len(heap)))
+
+# top = heappop(heap)    
+# print ("top node is {}".format(top.get_value()))    
 
 
-print(traverse(top, ""))
 
-print("Encoding table: ")
-for k, v in encode_table.items():
-    print("k {}: {}".format(k, v))
-
-
-encoded = ""
-for s in str_to_encode:
-    print(s)
-    encoded += encode_table[s]
-
-print(encoded)
 
 decode(encoded, top)
 
